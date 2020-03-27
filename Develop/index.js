90d69f243eef33247f58b5d68d1b32a91760eb45
nsakib <nat.sakib@gmail.com>
2020-03-26 23:09:11 -0400

    got write fs to work

--- Develop/index.js
+++ Develop/index.js
@@ -1,33 +1,94 @@
-// Import the package(s)
-const fs = require("fs");
 const inquirer = require("inquirer");
-const axios = require("inquirer")
+const fs = require("fs");
+const axios = require("axios");
 
-const questions = [{
-    type: "input",
-    message: "what is your GitHub username?",
-    name: "userName"
-},{
-    type: "input",
-    message: "Which repository would you like to generate a readme.md for?",
-    name: "userRepo"
-}];
+const questions = [
+"What is your GitHub user name?", 
+"What is the name of your repository?", 
+"Please input a description of your repository.",
+"Which of these licenses are applicable to your reposity?",
+"If there are dependencies, what command should a user input in order to install them?",
+"What do you want the user know about using this repo?",
+"What commmand should be used to run tests?",
+"What does the user need to know about contributing to the repo?"
+];
+inquirer.prompt([
+    {
+        type: "input",
+        message: questions[0],
+        name: "userName",
+    },
+    {
+        type: "input",
+        message: questions[1],
+        name: "title",
+    },
+    {
+        type: "input",
+        message: questions[2],
+        name: "description",
+    },
+    {
+        type: "list",
+        message: questions[3],
+        choices: [
+            "MIT",
+            "APACHE 2.0", 
+            "GPL 3.0", 
+            "BSD 3",
+            "None"
+          ],
+          name: "license"
+    },
+    {
+        type: "input",
+        message: questions[4],
+        name: "install",
+    },
+    {
+        type: "input",
+        message: questions[5],
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: questions[6],
+        name: "tests",
+    },
+    {
+        type: "input",
+        message: questions[7],
+        name: "contributions",
+    },
+]).then(function(data) {
+    const {userName, title, description, license, install, usage, tests, contributions} = data;
+    const queryUrl = `https://api.github.com/users/${userName}/repos?per_page=100`;
+    console.log(queryUrl);
+    axios.get(queryUrl).then(function(response) {
+        // const repoNames = response.data.map(function(repo) {
+        const userImg = (response.data[0].owner.avatar_url);
+        // return repoNames;
+      });
+    //   const repoNamesStr = repoNames.join("\n");
+      fs.writeFile("readme1.md", `
+      # ${userName} ![user_image](${userImg})
 
-inquirer.prompt.questions.forEach(element).then(function({ userName }){
-        const queryURL = `https://api.github.com/users/${userName}/repos?per_page=100`;
+      # ${title}
+      ## ${description}
 
-        axios.get(queryURL).then(function(res){
-            const repoNames = res.data.map(function(repo){
-                return repo.name;
-            })
-        })
-    })
+      ### ${license}
 
-function writeToFile(fileName, data) {
-}
+      ## ${install}
 
-function init() {
+      ## ${usage}
 
-}
+      ## ${tests}
 
-init();
+      ## ${contributions}
+     `, function(err) {
+        if (err) {
+          throw err;
+        }
+
+      });
+    });
