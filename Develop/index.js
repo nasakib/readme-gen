93d5c3b322cd6743a6e3b0a7e098c6dd90447cff
nsakib <nat.sakib@gmail.com>
2020-03-26 23:54:34 -0400

    Final stages

--- Develop/index.js
+++ Develop/index.js
@@ -8,9 +8,9 @@ const questions = [
 "Please input a description of your repository.",
 "Which of these licenses are applicable to your reposity?",
 "If there are dependencies, what command should a user input in order to install them?",
-"What do you want the user know about using this repo?",
-"What commmand should be used to run tests?",
-"What does the user need to know about contributing to the repo?"
+"Is there anything that should be said about using the repository?",
+"How should the repository be tested??",
+"What should be said about contributions to the repository??"
 ];
 inquirer.prompt([
     {
@@ -43,7 +43,7 @@ inquirer.prompt([
     {
         type: "input",
         message: questions[4],
-        name: "install",
+        name: "installDir",
     },
     {
         type: "input",
@@ -61,30 +61,42 @@ inquirer.prompt([
         name: "contributions",
     },
 ]).then(function(data) {
-    const {userName, title, description, license, install, usage, tests, contributions} = data;
+    const {userName, title, description, license, installDir, usage, tests, contributions} = data;
     const queryUrl = `https://api.github.com/users/${userName}/repos?per_page=100`;
     console.log(queryUrl);
     axios.get(queryUrl).then(function(response) {
         // const repoNames = response.data.map(function(repo) {
         const userImg = (response.data[0].owner.avatar_url);
         // return repoNames;
-      });
+     // }); 
     //   const repoNamesStr = repoNames.join("\n");
       fs.writeFile("readme1.md", `
-      # ${userName} ![user_image](${userImg})
+      # ${userName} 
+      <img src="${userImg}" alt="avatar" style="border-radios: 5px" width="40" />
 
-      # ${title}
-      ## ${description}
+      # Title
+      ${title}
 
-      ### ${license}
+      ## Description:
+      ${description}
 
-      ## ${install}
+      ### License
+      ${license} [![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+      
+      # Table of Contents:
 
-      ## ${usage}
+      ## Install Directions:
+      ${installDir}
 
-      ## ${tests}
+      ## Use specifications:
+      ${usage}
 
-      ## ${contributions}
+      ## Test Specifications:
+      ${tests}
+
+      ## Contributions:
+      ${contributions}
+
      `, function(err) {
         if (err) {
           throw err;
@@ -92,3 +104,5 @@ inquirer.prompt([
 
       });
     });
+  });//.catch(function(){
+      //console.log("Sorry, your input was not valid.")
